c83bf881a6e06853d41c94d905fc29869b031d50
AryaK19 <aryakadam348@gmail.com>
2024-09-19 09:00:08 +0530

    added requirments

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\arya2\Arya Code\Python\Machine Learning\NLP(spacy,nltk)\New folder\venv
+set VIRTUAL_ENV=C:\Users\arya2\Arya Code\Python\Machine Learning\NLP(spacy,nltk)\RetailX Project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
